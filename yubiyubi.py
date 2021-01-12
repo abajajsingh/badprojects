66fd927df26602167781f4d1832abc4990b6e1e3
Jack <49700385+goodnessidontknow@users.noreply.github.com>
2021-01-11 22:08:40 -0800

    Update yubiyubi.py

--- yubiyubi.py
+++ yubiyubi.py
@@ -1,22 +1,15 @@
 import praw
 
-reddit = praw.Reddit(client_id = 'DkLr0rfzwv47Wg',
-                     client_secret='NeDl34tBs3UX44EyAVrHFsTaDl0_4Q',
-                     user_agent='<console:yubi-bot:0.0.2>',
-                     username='****',
-                     password='****')
-reddit.validate_on_submit = True
-
 title = "Yubi yubi!"
 url = 'https://static.zerochan.net/Inugami.Korone.full.2982792.jpg'
 index = 0;
 
 while True:
-    reddit = praw.Reddit(client_id = 'DkLr0rfzwv47Wg',
-                         client_secret='NeDl34tBs3UX44EyAVrHFsTaDl0_4Q',
+    reddit = praw.Reddit(client_id = '****',
+                         client_secret='****',
                          user_agent=str(index),
-                         username='yubi-bot',
-                         password='90dogyears')
+                         username='****',
+                         password='****')
     reddit.validate_on_submit = True
     try:
         subreddit = reddit.subreddit('c4dgaming')
